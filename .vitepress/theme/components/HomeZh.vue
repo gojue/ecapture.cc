a30e6beb4dbbc88e7fd6b0672879087d48a336b1
cfc4n <cfc4n.cs@gmail.com>
2024-05-05 00:21:09 +0800

    remove `release` label of github version image

--- .vitepress/theme/components/HomeZh.vue
+++ .vitepress/theme/components/HomeZh.vue
@@ -27,7 +27,7 @@ import VueMasteryModalZh from './VueMasteryModalZh.vue';
           />
         </svg>
       </a>
-      <a class="setup" href="https://github.com/gojue/ecapture/releases" target="_blank">下载  <img src="https://img.shields.io/github/v/release/gojue/ecapture?display_name=tag&include_prereleases&sort=semver" alt="eCapture Version"/> </a>
+      <a class="setup" href="https://github.com/gojue/ecapture/releases" target="_blank">下载  <img src="https://img.shields.io/github/v/release/gojue/ecapture?display_name=tag&include_prereleases&sort=semver&label=" alt="eCapture Version"/> </a>
     </p>
   </section>
 
