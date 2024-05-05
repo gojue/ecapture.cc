2016edcd822c933f55342a7f07996998906c50a2
cfc4n <cfc4n.cs@gmail.com>
2024-05-05 11:41:26 +0800

    typos.

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -18,7 +18,7 @@ const nav = [
     link: '/'
   },
   { text: 'Guide', link: '/guide/introduction' },
-  { text: 'Develop', link: '/develop' },
+  { text: 'Develop', link: '/develop/' },
   {
     text: '中文',
     link: '/zh/',
